5a08d7492924e1bb44b3ef8653a193127055b549
MaheshChavan1264 <mahesh.chavan1264@gmail.com>
2021-10-25 22:14:44 +0530

    Added Color boxes

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import {useEffect, useState} from 'react';
+const width = 8
+const candyColors = [
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'red',
+    'yellow'
+]
+ const App = () => {
+    const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+    const randomColorArrangement = [];
+
+    const checkForColumnOfThree = () =>{
+        for(let i = 0; i < 47; i++){
+            const columnOfThree = [i, i + width, i + width * 2];
+            const decidedColor = currentColorArrangement[i];
+            if(columnOfThree.every(square => currentColorArrangement[square]=== decidedColor)){
+
+            }
+        }
+    }
+    const createBoard = () =>{
+        for(let i = 0; i < width * width; i++){
+            const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
+            randomColorArrangement.push(randomColor);
+        }
+        setCurrentColorArrangement(randomColorArrangement);
+    }
+
+    useEffect(() => {
+        createBoard();
+    },[])
+
+     console.log(currentColorArrangement);
+
+  return (
+      <div className="app">
+          <div className="game">
+              {currentColorArrangement.map((candyColor, index)=>(
+                  <img key = {index}
+                       style={{backgroundColor: candyColor}}
+                       alt={candyColor}
+                  />
+             ))}
+          </div>
+      </div>
+  );
+}
+
+export default App;
